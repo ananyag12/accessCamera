629bcca0903f354764552b276ee7c60350464ec3
Ananya Gollakota <ananya.gollakota@gmail.com>
2022-06-01 11:23:50 -0400

    testing how to access camera & display photos

--- accessCamera/ViewController.swift
+++ accessCamera/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
     }
 
-
+    @IBAction func choosePhotoTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated:true, completion: nil)
+        
+    }
+    
+    @IBOutlet weak var imageDisplay: UIImageView!
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageDisplay.image = selectedImage
+    }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 }
 
